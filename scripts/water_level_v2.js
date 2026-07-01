f44cb4c3d10732dcd274f399c8cb817f72840612
portobistineau <portobistineau@gmail.com>
2026-07-01 10:10:14 -0500

    Update water_level_v2.js

--- scripts/water_level_v2.js
+++ scripts/water_level_v2.js
@@ -278,35 +278,35 @@ function renderChart(labels, observedData, forecastData, waterFillData, lastTime
                             type: 'box',
                             yMin: 145,
                             yMax: 150,
-                            backgroundColor: 'rgba(220,38,38,0.18)',
+                            backgroundColor: 'rgba(220,38,38,0.25)',
                             borderWidth: 0
                         },
                         moderateBand: {
                             type: 'box',
                             yMin: 144,
                             yMax: 145,
-                            backgroundColor: 'rgba(244,114,182,0.18)',
+                            backgroundColor: 'rgba(244,114,182,0.25)',
                             borderWidth: 0
                         },
                         minorBand: {
                             type: 'box',
                             yMin: 142.5,
                             yMax: 144,
-                            backgroundColor: 'rgba(249,115,22,0.18)',
+                            backgroundColor: 'rgba(249,115,22,0.25)',
                             borderWidth: 0
                         },
                         actionBand: {
                             type: 'box',
                             yMin: 142,
                             yMax: 142.5,
-                            backgroundColor: 'rgba(250,204,21,0.18)',
+                            backgroundColor: 'rgba(250,204,21,0.25)',
                             borderWidth: 0
                         },
                         normalBand: {
                             type: 'box',
                             yMin: 141,
                             yMax: 142,
-                            backgroundColor: 'rgba(34, 197, 94, 0.18)',
+                            backgroundColor: 'rgba(34, 197, 94, 0.25)',
                             borderWidth: 0
                         },
                         forecastDivider: {
